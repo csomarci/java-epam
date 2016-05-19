66f7ea26762443ae8ceeba33b64f0b8c2ba96b26
Marton_Csordas <Marton_Csordas@epam.com>
2016-05-19 11:46:00 +0200

    HelloWorld package

---
+++ HelloWorld/com/epam/training/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.epam.training;
+
+/**
+ * This is my first Java class.<br>
+ * @author Marton Csordas
+ * @since 1.0
+ */
+
+ 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello "+System.getProperty("name")+ " World!\nThis is a new line" );
+    }
+
+}
